ef7457f587fd58692990ed5227d257904ff88d9c
bouje1831715 <bouje1831715@etu.cegepjonquiere>
2018-09-12 15:22:02 -0400

    superman krypte final

--- kryptonite/Program.cs
+++ kryptonite/Program.cs
@@ -6,7 +6,23 @@ namespace kryptonite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string phrase = "bonjour";
+            Random keyR = new Random();
+            int key = keyR.Next(-15, 16);
+            string krypter = "";
+
+
+
+            Console.WriteLine("Veuiller entrer la phrase a krypter");
+            phrase = Console.ReadLine();
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                krypter += Convert.ToChar((int)phrase[i] + key);
+            }
+            Console.WriteLine("votre phrase krypter est " + krypter + " avec une clé de " + key);
+
+            Console.ReadKey();
         }
     }
 }
